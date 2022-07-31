46fddcaa7b1a48428cfadc948f29676655bf4192
syahrulmf <syahrulmf@gmail.com>
2022-07-31 15:11:04 +0700

    Hero Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      ...colors,
+      pink: { ...colors.pink, 400: "#F9CADA" },
+      blue: { ...colors.blue, 200: "#BCE7F0" },
+    },
+    fontFamily: {
+      sans: ["'Open Sans', sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
